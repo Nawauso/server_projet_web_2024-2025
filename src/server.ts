63965a659b0d2d0c9addfdd037ef5b1f84fc518e
Daniel <la229726@student.helha.be>
2024-10-14 15:59:10 +0200

    sample response json for client

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT;
+const cors = require("cors");
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (request: Request, response: Response) => {
     response.status(200).send("Cocoup toi");
+});
+
+app.get("/api" , (request: Request, response: Response) => {
+    response.json({films: ["Matrix","Bob l'éponge","Joker"]})
 });
 
 app.listen(PORT, () => {
