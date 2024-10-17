8ec9605f9080fc484e7bb0c57a780e01eb1330ea
Daniel <la229726@student.helha.be>
2024-10-17 15:20:19 +0200

    .env sample + method test fetchTMDB

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import axios, {isCancel, AxiosError} from 'axios';
+import * as url from "node:url";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
-const cors = require("cors");
+const PORT = process.env.PORT || '8080';
+
 const corsOptions = {
     origin: ["http://localhost:5173"],
 };
+
+const requiredEnv = ["TMDB_URL","TMDB_TOKEN","DB_TYPE"]
+
+for (const env of requiredEnv) {
+    if(process.env[env] === undefined) {
+        throw new Error(`Missing required env: ${env}`);
+    }
+}
 
 app.use(cors(corsOptions));
 
@@ -24,3 +35,16 @@ app.listen(PORT, () => {
 }).on("error", (err: Error) => {
     throw new Error(err.message);
 });
+
+async function getTMDBList() {
+    try{
+        const response = await axios.get(`${process.env.TMDB_URL}`, {
+            method: 'GET',
+            headers: {accept: 'application/json', Authorization: `Bearer ${process.env.TMDB_TOKEN}`},
+        });
+        console.log(response);
+    }
+    catch(err){
+        console.error(err);
+    }
+}
