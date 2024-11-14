e68c25f17f08dab550b3fb1627aba80bfc62bfe8
SweaKz <la229726@student.helha.be>
2024-11-14 16:30:38 +0100

    Bug model réglé

--- src/models/Provider.ts
+++ src/models/Provider.ts
@@ -1,15 +1,15 @@
 
 export class Provider {
-    protected provider_id: number;
-    protected provider_name: string;
-    protected logo_path: string;
+    protected logo_Path: string;
+    protected provider_Name: string;
+    protected provider_ID: number;
 
-    constructor(provider_id: number, logo_path: string, provider_name: string) {
-        this.provider_id = provider_id;
-        this.provider_name = provider_name;
-        this.logo_path = logo_path;
+    constructor(logo_Path: string, provider_Name: string, provider_ID: number) {
+        this.logo_Path = logo_Path;
+        this.provider_Name = provider_Name;
+        this.provider_ID = provider_ID;
     }
     static fromJSON (data: any) : Provider {
-        return new Provider(data.provider_id, data.provider_name, data.logo_path );
+        return new Provider(data.logo_Path, data.provider_Name, data.provider_ID );
     }
 }
